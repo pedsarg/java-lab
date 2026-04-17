593462a478736a74364a0c50e00fe5d33459fb90
pedsarg <pedsar@gmail.com>
2026-04-17 07:10:06 -0300

    feat: add user insertion using in-memory database

--- Spring Boot/First Project - JPA | Hibernate/demo/src/main/java/com/example/demo/resources/UserResources.java
+++ Spring Boot/First Project - JPA | Hibernate/demo/src/main/java/com/example/demo/resources/UserResources.java
@@ -1,23 +1,37 @@
 package com.example.demo.resources;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entities.User;
+import com.example.demo.services.UserService;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResources {
+
+    @Autowired
+    private UserService service;
     
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "Bruna", "bruna@gmail.com", "9999999", "12345");
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
 
-        return ResponseEntity.ok().body(u);
+        return ResponseEntity.ok().body(list);
     }
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findByid(@PathVariable Long id) {
+        User obj = service.findByID(id);
 
+        return ResponseEntity.ok().body(obj);
+    }  
+    
 }
